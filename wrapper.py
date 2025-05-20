aa8124bc9e0e197bfb597db22e69cbd0ecc9d53e
jakeg00dwin <jakegoodwin@gorge.works>
2025-05-19 19:18:47 -0700

    Added some basic comments/sections and imported ctypes.

---
+++ wrapper.py
@@ -0,0 +1,21 @@
+import ctypes
+
+# Check what OS version is being used.
+
+# Attempt to Load The Shared Library
+
+# On failure to load the library return status.
+
+
+'''
+===========================================
+Helper Section.
+===========================================
+'''
+
+
+'''
+===========================================
+Class/Function Section.
+===========================================
+'''
