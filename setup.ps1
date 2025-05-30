a7592ec1ff23ceca58e733fafd29c7e821a660a0
jakeg00dwin <jakegoodwin@gorge.works>
2025-05-29 17:15:32 -0700

    Refactored the powershell script. Now it installs toolchain along with build tools.

--- setup.ps1
+++ setup.ps1
@@ -4,14 +4,27 @@
 # Description: Setup script to automate installation of dependencies.
 
 
-# First check if the script is being ran under admin.
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole('Administrator')) {
-    Write-Warning "You need to run this script as Administrator."
-    exit 1
+
+# #############################
+# Function Defs
+# #############################
+
+function Check-Requirements {
+    # First check if the script is being ran under admin.
+    if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole('Administrator')) {
+        Write-Warning "You need to run this script as Administrator."
+        exit 1
+    }
+
+    # Check if the tools/utilites needed are availble.
+    if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
+        Write-Error "winget is not installed. Install Windows Package Manager first."
+        exit 1
+    }
 }
 
 function Show-Menu {
-    Write-Host "\nSeeCube SDK - python bindings enviroment setup:"
+    Write-Host "`nSeeCube SDK - python bindings enviroment setup:"
     Write-Host "Installs: Git, CMake, Python3.x, OpenCV"
     Write-Host "1. Continue Installation"
     Write-Host "2. Exit"
@@ -24,17 +37,27 @@ function Show-Menu {
 }
 
 
-# Check if the tools/utilites needed are availble.
-if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
-    Write-Error "winget is not installed. Install Windows Package Manager first."
-    exit 1
+#Program Install Helper function.
+function Install-Package {
+    param (
+        [string]$PackageId,
+        [string]$DisplayName
+    )
+
+    Write-Host "`nInstalling $DisplayName"
+    winget install --id $Package -e --source winget
 }
 
 
-# Instal Git.
+# #############################
+# Start of main script.  
+# #############################
 
-# Install Python 3.10
+Check-Requirements
+Show-Menu
 
-# Install CMake
+Install-Package -PackageId "Git.Git" -DisplayName "Git"
+Install-Package -PackageId "Python.Python3.10" -DisplayName "Python 3.10"
+Install-Package -PackageId "Kitware.CMake" -DisplayName "CMake"
+Install-Package -PackageId "LLVM.LLVM" -DislpayName "LLVM/Clang"
 
-# Install 
