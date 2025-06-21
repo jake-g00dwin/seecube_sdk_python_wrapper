ca180297352575ad546802bbcf715e7e4f4a5eeb
jakeg00dwin <jakegoodwin@gorge.works>
2025-06-20 22:35:13 -0700

    refactored return type for tuple.

--- src/example.py
+++ src/example.py
@@ -184,7 +184,7 @@ def main():
     defective_pixels = None
     if(defective_pixels_correction):
         print("Defective pixel correction: Enabled")
-        defective_pixels = device.getDefectivePixelsCorrection()
+        (result, defective_pixels) = device.getDefectivePixelsCorrection()
     else:
         print("Defective pixel correction: Disabled")
 
