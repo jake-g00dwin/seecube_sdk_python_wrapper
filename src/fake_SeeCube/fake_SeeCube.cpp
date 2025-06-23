b819ecf04640ccc66b81842549b59b19c397614a
jakeg00dwin <jakegoodwin@gorge.works>
2025-06-22 20:54:08 -0700

    setup fake seecubesdk constructor and destructor.

--- src/fake_SeeCube/fake_SeeCube.cpp
+++ src/fake_SeeCube/fake_SeeCube.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "fake_SeeCube.h"
+#include <iostream>
 
 // dumb test function
 int add_two(int a)
@@ -13,4 +14,29 @@ int add_two(int a)
     int b = a;
     b += 2;
     return b;
+}
+
+
+/**
+* @brief Constructor for the SeeCubeSDK class.
+*
+* Creates a SeeCubeSDK instance and enumerates connected devices, setting up the SDK environment
+* with specified verbosity level and thread pool size.
+*
+* @param[in] pVerbosity The verbosity level for logging. Determines the amount of information logged.
+*                       Default value is verbosityLevel::error.
+* @param[in] pThreadPoolSize The size of the thread pool to be used by the SDK.
+*                            A value of 0 lets the SDK determine the optimal size.
+*                            Default is 0.
+*/
+SeeCubeSDK::SeeCubeSDK(const verbosityLevel& pVerbosity,
+           const int& pThreadPoolSize)
+{
+    std::cout << "Constructor Called!" << std::endl;
+}
+
+
+SeeCubeSDK::~SeeCubeSDK(void)
+{
+    std::cout << "Destructor Called!" << std::endl;
 }
--- src/fake_SeeCube/fake_SeeCube.h
+++ src/fake_SeeCube/fake_SeeCube.h
@@ -1,8 +1,15 @@
 /**
  * @file fake_SeeCube.h
- * @brief A fake of the SeeCube SDK 
+ * @brief A fake of the SeeCube SDK
  * @author: Jake Goodwin
  * @date: 2025-06-20
+ */
+
+/*
+ * This header is for faking the actual compiled SeeCube SDK that is
+ * distributed as a compiled library.
+ *
+ *
  */
 
 #ifndef FAKE_SEECUBE
@@ -12,4 +19,4 @@
 
 int add_two(int a);
 
-#endif //FAKE_SEECUBE   
+#endif // FAKE_SEECUBE
